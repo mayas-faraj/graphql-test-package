5bd8fb1c07a773e9863353f08b354c598fda9707
Mayas Faraj <uniqueprogrammer@hotmail.com>
2025-03-16 02:05:33 +0300

    remove clearning previous logs

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,5 @@
-import fs from "fs/promises";
-import { logDirName, createTestLogger } from "./logger.js";
+import { createTestLogger } from "./logger.js";
 export { default as performGraphqlTest, OperationType } from "./graphql-test.js";
 export { getServerData } from "./server-data.js";
 export { queryFirstId, queryLastId, queryNextId } from "./query-data.js";
 export { createTestLogger };
-
-// clear previous logs
-await fs.rm(logDirName, { force: true, recursive: true });
